cde645add9bff3800791f9f58e61658d23a992c8
shimpei1494 <one1218all38@icloud.com>
2023-01-08 23:07:40 +0900

    アニメの情報追加
    画像はImageを使いたかったが上手くいかずコメントアウト

--- components/AnimeArticle/index.jsx
+++ components/AnimeArticle/index.jsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import style from "./AnimeArticle.module.css"
 
 export function AnimeArticle(props) {
-
+const theAnime = props.oneAnime;
 
   return (
-    <li>{props.article.title}</li>
+    <li>
+      <div className={style.article}>
+        <p>{theAnime.title}</p>
+        <p>{theAnime.title_kana}</p>
+        <p>{theAnime.watchers_count}</p>
+        {/* <Image src={theAnime.images.recommended_url} width={64} height={64}/> */}
+        <img src={theAnime.images.recommended_url}></img>
+      </div>
+    </li>
   );
 }
