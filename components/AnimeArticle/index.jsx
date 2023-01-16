aa65e4f554bdb282b4cae6b6425ef8046613b2df
shimpei1494 <one1218all38@icloud.com>
2023-01-17 07:33:44 +0900

    各アニメ公式URLへのリンク作成

--- components/AnimeArticle/index.jsx
+++ components/AnimeArticle/index.jsx
@@ -8,8 +8,8 @@ export function AnimeArticle(props) {
     <li>
       <div className={style.article}>
         <h2>{theAnime.title}</h2>
-        <p>{theAnime.title_kana}</p>
         <p>wacth数：{theAnime.watchers_count}</p>
+        <p><a href={theAnime.official_site_url}>公式URL</a></p>
         {/* Imageコンポーネントを使用したいが、これを使用するとCORSのエラー出て、特定ドメインをしたいが、いろんなurlから画像を持ってきていると思われるのでどうしたらいいかという状況 */}
         {/* <Image src={theAnime.images.recommended_url} alt="no image" width={64} height={64}/> */}
         <img src={theAnime.images.recommended_url} alt="no image" width="30%" height="30%" onError={(e) => e.target.src = 'images/noimage-1024x898.png'}
