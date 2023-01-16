699a76c08dd9fca5b87de640742bcf76c99066bf
shimpei1494 <one1218all38@icloud.com>
2023-01-17 07:14:39 +0900

    各アニメの表示変更
    タイトルを大きく（h2）
    watch数をわかりやすく

--- components/AnimeArticle/index.jsx
+++ components/AnimeArticle/index.jsx
@@ -7,9 +7,9 @@ export function AnimeArticle(props) {
   return (
     <li>
       <div className={style.article}>
-        <p>{theAnime.title}</p>
+        <h2>{theAnime.title}</h2>
         <p>{theAnime.title_kana}</p>
-        <p>{theAnime.watchers_count}</p>
+        <p>wacth数：{theAnime.watchers_count}</p>
         {/* Imageコンポーネントを使用したいが、これを使用するとCORSのエラー出て、特定ドメインをしたいが、いろんなurlから画像を持ってきていると思われるのでどうしたらいいかという状況 */}
         {/* <Image src={theAnime.images.recommended_url} alt="no image" width={64} height={64}/> */}
         <img src={theAnime.images.recommended_url} alt="no image" width="30%" height="30%" onError={(e) => e.target.src = 'images/noimage-1024x898.png'}
