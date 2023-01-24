079e5f5bf922d7405419ab1d72fce235200d1997
shimpei1494 <one1218all38@icloud.com>
2023-01-24 23:10:00 +0900

    ダークモード切り替え可能に

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,10 +1,20 @@
-import { Box, Heading } from "@chakra-ui/layout";
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+import { Box, Center, Flex, Heading, Spacer } from "@chakra-ui/layout";
 
 export function Header() {
+  const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <Box bg="teal.400">
+    <Center h={100} bg="teal.400">
       <Heading color="white">アニメリスト検索アプリ</Heading>
-    </Box>
+      <IconButton
+        // _focus={{_focus: "none"}} //周りの青いアウトラインが気になる場合に消す方法
+        aria-label="DarkMode Switch"
+        icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+        onClick={toggleColorMode}
+      />
+    </Center>
   );
 }
