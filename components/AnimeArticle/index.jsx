d4dafd6ac34ae7ec0446f4351f014b2e7ff44467
shimpei1494 <one1218all38@icloud.com>
2023-01-26 07:38:42 +0900

    画像サイズなど修正

--- components/AnimeArticle/index.jsx
+++ components/AnimeArticle/index.jsx
@@ -1,18 +1,23 @@
-import { Box, Heading, Link, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/image";
+import { Box, Heading, HStack, Link, Text, VStack } from "@chakra-ui/layout";
 
 
 export function AnimeArticle(props) {
   const theAnime = props.oneAnime;
 
   return (
-    <Box bgColor="teal.300" p="2" m="2">
+    <Box bgColor="teal.300" p={2} my={8}>
       <Heading size='lg'>{theAnime.title}</Heading>
-      <Text>wacth数：{theAnime.watchers_count}</Text>
-      <Link href={theAnime.official_site_url} target='_blank'>公式URL</Link>
-      {/* Imageコンポーネントを使用したいが、これを使用するとCORSのエラー出て、特定ドメインをしたいが、いろんなurlから画像を持ってきていると思われるのでどうしたらいいかという状況 */}
-      {/* <Image src={theAnime.images.recommended_url} alt="no image" width={64} height={64}/> */}
-      <img src={theAnime.images.facebook.og_image_url} alt="no image" width="30%" height="30%" onError={(e) => e.target.src = 'images/noimage-1024x898.png'}
-      ></img>
+      <HStack py={5}>
+        <VStack>
+          <Text>wacth数：{theAnime.watchers_count}</Text>
+          <Link href={theAnime.official_site_url} target='_blank'>公式URL</Link>
+        </VStack>
+        {/* Imageコンポーネントを使用したいが、これを使用するとCORSのエラー出て、特定ドメインをしたいが、いろんなurlから画像を持ってきていると思われるのでどうしたらいいかという状況 */}
+        {/* <Image src={theAnime.images.recommended_url} alt="no image" width={64} height={64}/> */}
+        <Image src={theAnime.images.facebook.og_image_url} alt="no image" width={"70%"} height={"70%"} onError={(e) => e.target.src = 'images/noimage-1024x898.png'}
+        ></Image>
+      </HStack>
     </Box>
   );
 }
