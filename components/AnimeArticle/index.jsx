fc62bb291030ebd5e316b16dfe52c3d97de43d6f
shimpei1494 <one1218all38@icloud.com>
2023-01-28 13:02:11 +0900

    アニメ記事の修正

--- components/AnimeArticle/index.jsx
+++ components/AnimeArticle/index.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/button";
 import { useDisclosure } from "@chakra-ui/hooks";
 import { Image } from "@chakra-ui/image";
-import { Box, Heading, HStack, Link, Text, VStack } from "@chakra-ui/layout";
+import { Box, Heading, HStack, Link, ListItem, Text, UnorderedList, VStack } from "@chakra-ui/layout";
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
 
 
@@ -14,13 +14,13 @@ export function AnimeArticle(props) {
       <Heading size='lg'>{theAnime.title}</Heading>
       <HStack py={5}>
         <VStack>
-          <Text>wacth数：{theAnime.watchers_count}</Text>
-          <Link href={theAnime.official_site_url} target='_blank'>公式ページ</Link>
-          <Link href={theAnime.wikipedia_url} target='_blank'>Wikipedia</Link>
+          <UnorderedList>
+            <ListItem>wacth数：{theAnime.watchers_count}</ListItem>
+            <ListItem><Link href={theAnime.official_site_url} target='_blank'>公式ページ</Link></ListItem>
+            <ListItem><Link href={theAnime.wikipedia_url} target='_blank'>Wikipedia</Link></ListItem>
+          </UnorderedList>
           <Button onClick={onOpen} border="1px">詳細</Button>
         </VStack>
-        {/* Imageコンポーネントを使用したいが、これを使用するとCORSのエラー出て、特定ドメインをしたいが、いろんなurlから画像を持ってきていると思われるのでどうしたらいいかという状況 */}
-        {/* <Image src={theAnime.images.recommended_url} alt="no image" width={64} height={64}/> */}
         <Image src={theAnime.images.facebook.og_image_url} alt="アニメの画像" width={"70%"} height={"70%"} onError={(e) => e.target.src = 'images/no_image_yoko.jpg'}
         ></Image>
       </HStack>
