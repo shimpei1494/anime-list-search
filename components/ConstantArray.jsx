a3d9e40faa275cb7dde1f55527c8d51bed80b516
shimpei1494 <one1218all38@icloud.com>
2023-01-28 11:21:59 +0900

    シーズン情報を定数として出力

--- components/ConstantArray.jsx
+++ components/ConstantArray.jsx
@@ -11,5 +11,37 @@ const yearArray = (a, z) => {
   }
   return list;
 };
-// 現在の年+1までを配列としてエクスポート
-export const Years = yearArray(yearStart, nowYear)
+
+
+// 現在のSeason（春=0、夏=1、秋=2、冬期=3）
+let nowSeasonNum = ""
+const nowMonth = now.getMonth();
+switch (nowMonth) {
+  case 0:
+  case 1:
+  case 2:
+    nowSeasonNum = 3;
+    break
+  case 3:
+  case 4:
+  case 5:
+    nowSeasonNum = 0;
+    break
+  case 6:
+  case 7:
+  case 8:
+    nowSeasonNum = 1;
+    break
+  case 9:
+  case 10:
+  case 11:
+    nowSeasonNum = 2;
+    break
+}
+
+// シーズンのvalueと表示文字用の配列
+const seasonArray = [["spring", "春シーズン"],["summer", "夏シーズン"],["autumn", "秋シーズン"],["winter", "冬シーズン"]];
+
+// yearArray:現在の年+1までを配列,nowYear:今年
+const Years = {yearOption: yearArray(yearStart, nowYear), nowYear: nowYear};
+export {Years, nowSeasonNum, seasonArray};
