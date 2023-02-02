7e0255f0654dbeff5019f46732d527ffc00550aa
shimpei1494 <one1218all38@icloud.com>
2023-02-02 23:03:00 +0900

    しょぼいカレンダーapiのcorsエラー解消

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://cal.syoboi.jp/:path*', 
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
