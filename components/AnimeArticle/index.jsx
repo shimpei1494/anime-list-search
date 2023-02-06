538f967fdd7063453eb79d444f16a22b187ae998
大下慎平 <oshita@fundely.co.jp>
2023-02-06 17:33:07 +0900

    改行も綺麗に取り除く

--- components/AnimeArticle/index.jsx
+++ components/AnimeArticle/index.jsx
@@ -23,7 +23,7 @@ export function AnimeArticle(props) {
     const dataComment = dataJson.TitleLookupResponse.TitleItems.TitleItem.Comment._text;
     // 「*キャスト」で始まり、「*」で終わる部分を取り出す
     const cast = dataComment.match(/\*キャスト[\s\S]*\*/);
-    const castList = cast[0].replace(/\*キャスト/,"");
+    const castList = cast[0].replace(/\*キャスト\r\n/,"").replace(/\r\n\*/,"");
     console.log(castList);
   };
 
