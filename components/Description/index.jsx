1b393beb36469f23eb94b3157c06f363e3d54d71
大下慎平 <oshita@fundely.co.jp>
2023-02-07 18:20:30 +0900

    説明部分の修正と情報追加

--- components/Description/index.jsx
+++ components/Description/index.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@chakra-ui/button";
 import { useDisclosure } from "@chakra-ui/hooks";
-import { Box, Link, ListItem, Text, UnorderedList } from "@chakra-ui/layout";
+import { Box, Heading, Link, ListItem, Text, UnorderedList } from "@chakra-ui/layout";
 import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
 
 export function Description() {
@@ -14,19 +14,21 @@ export function Description() {
       <Modal isOpen={isOpen} onClose={onClose} size="xl">
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader textDecoration="underline">アニメリストの表示基準</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={10}>
-          <Text>watch数 ＝ annictで「見てる/見たい/見た」に登録している人の数</Text>
-          <Text>人気順をwatch数で判断し、降順で表示します</Text>
+          <Heading textDecoration="underline" size='lg' p={3}>アニメリストの表示基準</Heading>
+          <Text>watch数 ＝ annictで「見てる/見たい/見た」に登録している人の数。</Text>
+          <Text>人気順をwatch数で判断し、降順で表示します。</Text>
           <br/>
-          <Text>デフォルトでは現在の年、シーズンが選択されています</Text>
+          <Text>デフォルトでは現在の年、シーズンが選択されています。</Text>
           <UnorderedList>
             <ListItem>冬シーズン＝1~3月</ListItem>
             <ListItem>春シーズン＝4~6月</ListItem>
             <ListItem>夏シーズン＝7~9月</ListItem>
             <ListItem>秋シーズン＝10~12月</ListItem>
           </UnorderedList>
+          <Heading textDecoration="underline" size='lg' p={3}>詳細について。</Heading>
+          <Text>詳細ではキャストの情報も閲覧することができます</Text>
         </ModalBody>
       </ModalContent>
     </Modal>
