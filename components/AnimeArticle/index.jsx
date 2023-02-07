6b696a437173ff10135e4eb335712e9649ed8b40
大下慎平 <oshita@fundely.co.jp>
2023-02-07 17:56:59 +0900

    エラー時の処理を記載
    その他軽微な変更あり

--- components/AnimeArticle/index.jsx
+++ components/AnimeArticle/index.jsx
@@ -1,47 +1,47 @@
 import { Button } from "@chakra-ui/button";
 import { useDisclosure } from "@chakra-ui/hooks";
 import { Image } from "@chakra-ui/image";
-import { Box, Heading, HStack, Link, ListItem, Text, UnorderedList, VStack } from "@chakra-ui/layout";
-import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/modal";
+import { Box, Heading, HStack, Link, ListItem, UnorderedList, VStack } from "@chakra-ui/layout";
+import { Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalOverlay } from "@chakra-ui/modal";
 import { Tag } from "@chakra-ui/tag";
 import { useState } from "react";
 
 
 export function AnimeArticle(props) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const theAnime = props.oneAnime;
   const tid = props.syobocal_tid
-  const { isOpen, onOpen, onClose } = useDisclosure()
   const [castList, setCastList] = useState([]);
 
+  // しょぼいカレンダーからキャスト情報を持ってくる
   const searchShobocal = async (tid) => {
-    // devとproductionでドメインを場合分けする→しょぼいカレンダーのAPIをたたく
-    const domain = process.env.NODE_ENV === "production" ? "https://anime-list-search-nine.vercel.app/api" : "http://localhost:3000/api";
-    const response = await fetch(`${domain}/db.php?Command=TitleLookup&TID=${tid}`);
-    const dataText = await response.text();
+    try {
+      // devとproductionでドメインを場合分けする→しょぼいカレンダーのAPIをたたく
+      const domain = process.env.NODE_ENV === "production" ? "https://anime-list-search-nine.vercel.app/api" : "http://localhost:3000/api";
+      const response = await fetch(`${domain}/db.php?Command=TitleLookup&TID=${tid}`);
+      const dataText = await response.text();
 
-    // xmlで受け取ったテキストデータをjsonテキストに変換→コメントデータ部分のテキストを取り出す
-    const convert = require('xml-js');
-    const dataJsonText = convert.xml2json(dataText, {compact: true, spaces: 4});
-    const dataJson = JSON.parse(dataJsonText)
-   if (!dataJson) {
-    setCastList(["キャスト情報が見つかりません"]);
-   };
-    const dataComment = dataJson.TitleLookupResponse.TitleItems.TitleItem.Comment._text;
-    // キャスト情報の抜き出し（キャスト情報の後にテキストがある場合とない場合の２パターンがある）
-    let dataCast = []
-    if (dataComment.match(/\*キャスト[\s\S]*\*/)) {
-      dataCast = dataComment.match(/\*キャスト[\s\S]*\*/);
-    } else {
-      dataCast = dataComment.match(/\*キャスト[\s\S]*/);
-    } 
-    if (!dataCast | dataCast === []){
-      return setCastList(["キャスト情報が見つかりません"]);
+      // xmlで受け取ったテキストデータをjsonテキストに変換→コメントデータ部分のテキストを取り出す
+      const convert = require('xml-js');
+      const dataJsonText = convert.xml2json(dataText, {compact: true, spaces: 4});
+      const dataJson = JSON.parse(dataJsonText)
+      const dataComment = dataJson.TitleLookupResponse.TitleItems.TitleItem.Comment._text;
+      // キャスト情報の抜き出し（キャスト情報の後にテキストがある場合とない場合の２パターンがある）
+      let dataCast = []
+      if (dataComment.match(/\*キャスト[\s\S]*\*/)) {
+        dataCast = dataComment.match(/\*キャスト[\s\S]*\*/);
+      } else {
+        dataCast = dataComment.match(/\*キャスト[\s\S]*/);
+      } 
+      // 不要な部分を取り除く
+      const castText = dataCast[0].replace(/\*キャスト\r\n/,"").replace(/\r\n\*/,"")
+      // 改行を区切りとして配列に変換する
+      setCastList(castText.split(/\r\n/));
+    } catch {
+      setCastList(["キャスト情報が見つかりません"]);
     }
-    // 不要な部分を取り除く
-    const castText = dataCast[0].replace(/\*キャスト\r\n/,"").replace(/\r\n\*/,"")
-    // 改行を区切りとして配列に変換する
-    setCastList(castText.split(/\r\n/));
   };
+
 
   return (
     <Box bgColor="teal.300" p={2} my={8}>
@@ -63,7 +63,7 @@ export function AnimeArticle(props) {
       <Modal isOpen={isOpen} onClose={onClose} size="3xl">
         <ModalOverlay />
         <ModalContent>
-          <Heading size='xl' p={3}>{theAnime.title}</Heading>
+          <Heading size='xl' px={12} py={3}>{theAnime.title}</Heading>
           <ModalCloseButton />
           <ModalBody>
             <Image src={theAnime.images.facebook.og_image_url} alt="アニメの画像" width="100%" onError={(e) => e.target.style.display="none"}
